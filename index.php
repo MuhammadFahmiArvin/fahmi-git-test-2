73120852a32971d210a62d635704ab7e1e3a7688
m.fahmi arvin <fahmiarvin@gmail.com>
2024-02-05 11:13:27 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus, itaque illo provident perspiciatis maxime quisquam. Eligendi architecto nulla necessitatibus odio ullam veritatis itaque dolorum excepturi delectus, sint, ad quis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
